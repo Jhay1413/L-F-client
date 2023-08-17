7b909c9b217e0dc2d42b51732cb013b7fb424131
Jhay1413 <jhonchristian.ubaldo@evsu.edu.ph>
2023-08-18 07:38:00 +0800

    admin login

--- src/components/AuthPage/LoginPage.jsx
+++ src/components/AuthPage/LoginPage.jsx
@@ -1,7 +1,31 @@
-import {Routes,Route,Link } from 'react-router-dom'
+import { useState } from 'react';
+import {Routes,Route,Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import {toast,ToastContainer} from 'react-toastify';
 const LoginPage = () => {
+    const navigate = useNavigate()
+    const [user,setUser] = useState();
+    const [password,setPassword] = useState();
+
+    const showToast = (status,message)=>{
+        toast[status](message);
+      }
+      
+          
+
+
+    const login = ()=>{
+        console.log(user,password)
+        if(user === "admin" && password === "admin"){
+            navigate('/admin')
+        }
+        else{
+            showToast('error','Incorrect username or password !')
+        }
+    }
     return ( 
         <>
+           <ToastContainer/>
             <div className="w-full h-full flex items-center justify-center flex-col space-y-4 ">
                 <div className="flex flex-col items-center space-y-4">
                     <h1 className="text-4xl text-bold">Login Page</h1>
@@ -10,12 +34,12 @@ const LoginPage = () => {
                 
                 <div className="flex flex-col space-y-4 w-3/4">
 
-                    <input className="text-2xl rounded-lg border-2 p-2" type="Text" placeholder="User ID"/>
-                    <input className="text-2xl rounded-lg border-2 p-2" type="password" placeholder="Password"/>
+                    <input className="text-2xl rounded-lg border-2 p-2" type="Text" placeholder="User ID" onChange={(e)=>setUser(e.target.value)}/>
+                    <input className="text-2xl rounded-lg border-2 p-2" type="password" placeholder="Password"onChange={(e)=>setPassword(e.target.value)}/>
                 </div>
                 <div className="w-3/4 bg-blue-500 p-2 rounded-lg text-white text-2xl">
                
-                <Link to="/admin"><button className="w-full">LOGIN</button></Link>
+                <button className="w-full" onClick={login}>LOGIN</button>
                 </div>
             </div>
         </>
